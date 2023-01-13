163537bfdb6b15ae5b64ce21142817abce431a9d
donatomontrone <donato.montrone@outlook.com>
2023-01-13 13:01:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import { store } from './store.js';
+import axios from 'axios';
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store,
+      apiUrl: "apiurl",
+    }
+  },
+  methods: {
+    getAjaxCall() {
+      axios.get(this.apiUrl, {
+        params: {
+        }
+      })
+        .then((response) => {
+          // this.store.arraydovesalvare = (response.data.'cercare il dato che mi interessa');
+
+        })
+        .catch(function (error) {
+          // handle error
+          console.error(error);
+        })
+    }
+  },
+  created() {
+    this.getAjaxCall();
+  }
+}
+</script>
+<template>
+  <h1>App.vue</h1>
+</template>
+
+<style lang="scss">
+@use './styles/partials/variables.scss' as *;
+@use './styles/general.scss' as *;
+@use 'bootstrap/scss/bootstrap.scss' as *;
+</style>
