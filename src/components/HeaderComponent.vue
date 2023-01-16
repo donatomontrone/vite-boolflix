5152e5e411860f6e87e55c6682b66ebd557a069f
donatomontrone <donato.montrone@outlook.com>
2023-01-16 17:28:26 +0100

    add header styles

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,11 +3,12 @@
         <!-- Inseirire l'header con la search bar  -->
         <nav class="navbar bg-black">
             <div class="container-fluid">
-                <a class="navbar-brand text-white">Navbar</a>
+                <a class="navbar-brand text-white"><img src="../../public/boolflix.png" alt="Boolflix Logo"></a>
                 <div class="d-flex" role="search">
                     <input class="form-control me-2" placeholder="Search" v-model="userInput"
                         @keyup.enter="$emit('searchMovie', userInput)">
-                    <a class="btn btn-outline-success" @click="$emit('searchMovie', userInput)">Search</a>
+                    <a class="btn text-white" @click="$emit('searchMovie', userInput)"><i
+                            class="fa-solid fa-magnifying-glass"></i></a>
                 </div>
             </div>
         </nav>
@@ -26,5 +27,9 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
+nav img {
+    width: 120px;
+}
 </style>
